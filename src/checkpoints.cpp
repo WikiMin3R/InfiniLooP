91fa50cc685e65f994f63c71ea750eb3355c1d02
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2022-05-21 11:16:45 -0400

    Updated checkpoints to block 675001

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,7 +31,11 @@ static const int nCheckpointSpan = 500;
         ( 7500,  uint256("0x097cb27ca7f98df07fcb5b069f3252f636dc2842a18fdb90dc1c3fca2ac7f462") ) // last pow block
         ( 27310,  uint256("0xb7195b00e1ae01a42bf43b6469a1a9297265e12daebbbeaa26b15cd5001a1ef4") ) // January 31, 2021 checkpoint
         ( 318599,  uint256("0xef2c3952ea2a63f40e5a2498f6732386c768f2727021e381f43a26f36efa940e") ) // September 1, 2021 checkpoint
-    ;
+        ( 400001,  uint256("0x54fac03ec53c1eb81df641a6db15725bfa7113edbc9a2ba905e6d265db2069c7") ) // October 30, 2021 checkpoint
+        ( 500001,  uint256("0x65c1ca4b5c8a68af0cb2fa972d6e3540b03c5ca1c0766da5c1e79f682a9c8e5d") ) // January 10, 2022 checkpoint
+        ( 600001,  uint256("0x279f4cf9848fd39a06d054bdd37e1a0d5f814e4b3d2eb9244d872f3a2a4c64db") ) // March 24, 2022 checkpoint
+        ( 675001,  uint256("0xf824d1fad4373c2ecd1a0f97c0ed950df8acfb79cd4d7606169e2caa5e0a9e79") ) // May 18, 20221 checkpoint
+;
 
     // TestNet has no checkpoints
     static MapCheckpoints mapCheckpointsTestnet;
